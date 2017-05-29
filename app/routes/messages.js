06690addc49ff8148e82475047daba9f2dae0d5a
Martin Hesslund <martin.hesslund@callistaenterprise.se>
2017-05-30 00:38:03 +0200

    3.3

--- app/routes/messages.js
+++ app/routes/messages.js
@@ -3,8 +3,6 @@ import Ember from 'ember';
 export default Ember.Route.extend({
 
   model() {
-    return Ember.$.getJSON('/api/messages').then(function(data) {
-      return data.messages;
-    });
+    return this.store.query('message', {});
   }
 });
--- app/routes/messages/message.js
+++ app/routes/messages/message.js
@@ -3,8 +3,6 @@ import Ember from 'ember';
 export default Ember.Route.extend({
 
   model(params) {
-    return Ember.$.getJSON('/api/messages/' + params.message_id).then(function(data) {
-      return data.message;
-    })
+    return this.store.findRecord('message', params.message_id);
   }
 });
