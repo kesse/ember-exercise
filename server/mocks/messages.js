06e67da02f03e97303e70fd650618ec33034e335
Martin Hesslund <martin.hesslund@callistaenterprise.se>
2017-05-29 22:48:19 +0200

    Mock for messages

---
+++ server/mocks/messages.js
@@ -0,0 +1,39 @@
+module.exports = function(app) {
+  var messages = [{
+    id: 1,
+    title: 'Välkommen',
+    body: 'Välkommen till mitt system'
+  },{
+    id: 2,
+    title: 'Nyheter',
+    body: 'Här kommer lite nyheter'
+  },{
+    id: 3,
+    title: 'Ny model',
+    body: 'Den nya modellen fungerar väldigt bra'
+  }];
+
+   app.get('/api/messages', function(req, res) {
+     res.send({
+       messages: messages
+     });
+   });
+
+   app.get('/api/messages/:id', function(req, res) {
+
+     var id = req.params.id;
+
+    for (var i = 0; i < messages.length; i++) {
+      var message = messages[i];
+      if (message.id+"" === id) {
+        res.send({
+          message: message
+        });
+
+        return;
+      }
+    }
+
+     res.send({});
+   });
+};
