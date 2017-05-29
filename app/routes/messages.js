d0ae15d5c7b7e604b3b98e9f85b1169107cd12de
Martin Hesslund <martin.hesslund@callistaenterprise.se>
2017-05-29 23:19:37 +0200

    2.4

--- app/routes/messages.js
+++ app/routes/messages.js
@@ -1,4 +1,14 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+
+  model() {
+    return [{
+      id: 1,
+      title: 'Hej'
+    },{
+      id: 2,
+      title: 'Vad bra det går'
+    }]
+  }
 });
